b44fda206efa0a92c979cefbbcb24cce69d358a2
zhuchentong <2037630@163.com>
2023-03-30 16:22:38 +0800

    update

--- composables/use-export.ts
+++ composables/use-export.ts
@@ -2,7 +2,10 @@ import domToImage from "dom-to-image";
 
 export async function exportToPng(element: HTMLDivElement) {
   domToImage
-    .toPng(element, { style: { backgroundColor: "#fff" }, quality: 1 })
+    .toPng(element, {
+      style: { backgroundColor: "#fff", borderRadius: "10px" },
+      quality: 1,
+    })
     .then(function (dataUrl) {
       var link = document.createElement("a");
       link.download = "chat-export.png";
